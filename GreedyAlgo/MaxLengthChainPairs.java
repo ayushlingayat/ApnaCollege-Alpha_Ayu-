3c3f2217fd43ee2045dba716c6f2eab1f6f4ae62
Ayush Shyam Lingayat <lingayatayush3@gmail.com>
2025-05-13 15:45:28 +0530

    Greedy

--- GreedyAlgo/MaxLengthChainPairs.java
+++ GreedyAlgo/MaxLengthChainPairs.java
@@ -1,3 +1,24 @@
+import java.util.*;
+
 public class MaxLengthChainPairs {
+    public static void main(String args[]){ // 0(n log n)
+    int pairs[][] = {{5,24} , {39,60}, {5,28}, {27,40}, {50,90}};
+    Arrays.sort(pairs , Comparator.comparingDouble(o -> o[1]));
+
+    int chainLen = 1;
+    int chainEnd = pairs[0][1]; //Last selected chain end //chain end 
+
+    for(int i=1; i< pairs.length; i++){
+        if(pairs[i][0] > chainEnd){
+            chainLen++;
+            chainEnd = pairs[i][1];
+        }
+    }
+
+    System.out.println("Maximum Length of chain = " + "  " + chainLen);
+
+    }
+
     
 }
+
